ed9cd2805962dab6b5eb5475d5141699ef9c90a9
Eric Nelson II <nelsoneric2011@gmail.com>
2018-10-03 16:16:47 -0700

    display lives count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,61 @@
-// create an array of words
-var words = [
-  "link", 
-  "sonic", 
-  "kirby", 
-  "mario", 
-  "samus", 
-  "ness", 
-  "pikachu",
-  "yoshi",
-]
+(function () {
+  "use strict";
+    var availableLetters, words, guessInput, guess, guessButton, lettersGuessed, lettersMatched, output, man, letters, lives, currentWord, numLettersMatched, messages;
+    function setup() {
+      // create an array of words
+      words = [
+        "link", 
+        "sonic", 
+        "kirby", 
+        "mario", 
+        "samus", 
+        "ness", 
+        "pikachu",
+        "yoshi",
+      ]
+      // all avaible letters 
+      availableLetters = "abcdefghijklmnopqrstuvwxyz";
+      // player lives 
+      lives = 10;
+      // pop up messeges on screen
+      messages = {
+        win: "You win!",
+        lose: "Game over!",
+        guessed: "You fool! You already used that letter. Hahaha I stomped you.",
+        validLetter: "Enter a letter A-Z"
+      };
+
+      lettersGuessed = lettersMatched = " ";
+      numLettersMatched = 0;
+
+      // choose a word
+      currentWord = words[Math.floor(Math.random() * words.length)];
+      
+      // create var for later
+      output = document.getElementsByID("output");
+      man = document.getElementById("man");
+      guessInput = document.getElementById("letter");
+      
+      //  dislpay current lives 
+      man.innerHTML = "You have" + lives + " lives remaining";
+      output.innerHTML = " ";
+
+      document.getElementById ()
+    }
+})
+
+
+
+
+
+
+
+
+
+  
+}
+
+
 // choose random word
 var word = words[Math.floor(Math.random() * words.length)];
 // set up answer array
