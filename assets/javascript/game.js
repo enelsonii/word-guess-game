91b9c2401e26b685ebbda0185d8f96d5b7f7ce0e
Eric Nelson II <nelsoneric2011@gmail.com>
2018-10-03 16:07:07 -0700

    added buttons to html. created function for javascript, able to select random words.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,48 @@
 // create an array of words
-var word = [
+var words = [
   "link", 
   "sonic", 
-  "kratos", 
+  "kirby", 
   "mario", 
   "samus", 
-  "master chief", 
-  "soild snake",
-  "donkey kong",
+  "ness", 
+  "pikachu",
+  "yoshi",
 ]
 // choose random word
-var randomWord = Math.floor(Math.random() * word.length);
-var chosenWord = word[randomWord]; 
-var underScores = [];
-console.log(chosenWord);
-// create underscores based on word
-var generateUnderscore = () => {
-  for (var i = 0; i < chosenWord.length; i++){
-     underScores.push("_");
-  }
-  return underScores;
+var word = words[Math.floor(Math.random() * words.length)];
+// set up answer array
+var answerArray = [];
+for(var i = 0; i < word.length; i++) {
+  answerArray[i] = "_"
 }
 
-console.log(generateUnderscore());
-//  get users guess
-document.addEventListener("keypress", (event) => {
-  var keyword = String.fromCharCode(keyCode);
-  console.log(keyword)
-});
-//  check if guess is right 
-// if right pish to right array
-// if wrong push to wrong array
- 
+var remainingLetters = word.length;
+
+// the game loop 
+while(remainingLetters > 0) {
+  // show the player progress
+  document.write(answerArray.join(" "));
+
+  // get guess from the player
+  var guess = document.write("Guess a letter, or click to quit game.");
+  if (guess === null) {
+    // end the game loop
+    break;
+  }
+  // check for these conditions, ensuring that guess is exactly one letter.
+  else if (guess.length !== 1) {
+    document.write("please enter single letter.");
+  }
+  else {
+    // update the game stats with player guesses
+    for (var j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+      answerArray[j] = guess;
+      remainingLetters
+    }
+  }
+}
+}
+
+
