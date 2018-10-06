a04ef14621dbf86d1ebe38ce6908a6a7485f37d4
Eric Nelson II <nelsoneric2011@gmail.com>
2018-10-05 22:31:59 -0700

    Created functional javascrip. application able to tak input.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,62 @@
-//create array of words
+// creating varibles
 var words = [
   "mario",
   "link",
   "samus",
-  "ness",
-  "snake",
+  "pikachu",
+  "kirby",
   "fox",
-  "pit",
-  "falco",
-  "pikachu"
+  "lucas"
 ];
-//   choose random word
-var word = words[Math.floor(Math.random() * words.length)];
+var wins = 0;
+var loss = 0;
+var wrongLetter = [];
+var guessesLeft = 10;
+var underScores = [];
+var userGuesses = [];
+var word;
+var winCounter = 0;
 
+// function 
+// whenever start game is called, a new random word will be selected
+function startGamne() {
+  // picks a random word
+  word = words[Math.floor(Math.random() * words.length)];
+  console.log("random word = " + word);
+  for (var i = 0; i < word.length; i++) {
+    underScores.push("_");
+  }
+  // displaying underscores
+  document.getElementById("word-blanks").textContent = underScores.join(" ");
+  //  reset
+  wrongLetter = [];
+  guessesLeft = 10;
 
-//set up answer array 
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-  
+  //HTML
+  document.getElementById("guess-left").textContent = guessesLeft;
+
 }
-console.log(answerArray);
+function winLose() {
+  if  
+}
+// Player Guesses
+document.onkeyup = function (event) {
+  userGuesses = event.key;
+  // check if the letter exist inside of the word 
+  if (word.indexOf(userGuesses) > -1) {
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] === userGuesses) {
+        underScores[i] = userGuesses;
+        console.log(underScores);
+      }
+    }
+  }
+  else {
+    wrongLetter.push(userGuesses);
+    guessesLeft--;
+    console.log(wrongLetter);
+  }
+}
+startGamne();
+// 
+console.log(word[i]); 
