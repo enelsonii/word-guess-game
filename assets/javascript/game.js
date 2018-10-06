2dffb04f5dc0018fb456b04dc6dd511759019faf
Eric Nelson II <nelsoneric2011@gmail.com>
2018-10-05 23:52:41 -0700

    Java still buggy. Start button functional but user guess has no response.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,13 +19,13 @@ var winCounter = 0;
 
 // function 
 // whenever start game is called, a new random word will be selected
-function startGamne() {
+function startGame() {
   // picks a random word
   word = words[Math.floor(Math.random() * words.length)];
   console.log("random word = " + word);
   for (var i = 0; i < word.length; i++) {
     underScores.push("_");
-  }
+  } 
   // displaying underscores
   document.getElementById("word-blanks").textContent = underScores.join(" ");
   //  reset
@@ -35,28 +35,38 @@ function startGamne() {
   //HTML
   document.getElementById("guess-left").textContent = guessesLeft;
 
-}
-function winLose() {
-  if  
-}
-// Player Guesses
-document.onkeyup = function (event) {
-  userGuesses = event.key;
-  // check if the letter exist inside of the word 
-  if (word.indexOf(userGuesses) > -1) {
-    for (var i = 0; i < word.length; i++) {
-      if (word[i] === userGuesses) {
-        underScores[i] = userGuesses;
-        console.log(underScores);
+  // 
+  function winLose() {
+    if (winCounter === word.length) {
+      alert("You win!");
+      
+    }
+    else if (guessesLeft === 0) {
+      alert("You lose!");
+      
+    }
+    // Player Guesses
+    document.onkeyup = function (event) {
+      userGuesses = event.key;
+      // check if the letter exist inside of the word 
+      if (word.indexOf(userGuesses) > -1) {
+        for (var i = 0; i < word.length; i++) {
+          if (word[i] === userGuesses) {
+            underScores[i] = userGuesses;
+            console.log(underScores);
+            winCounter++;
+            winLose();
+          }
+        }
+      }
+      else {
+        wrongLetter.push(userGuesses);
+        guessesLeft--;
+        console.log(wrongLetter);
+        winLose();
       }
     }
   }
-  else {
-    wrongLetter.push(userGuesses);
-    guessesLeft--;
-    console.log(wrongLetter);
-  }
 }
-startGamne();
+
 // 
-console.log(word[i]); 
